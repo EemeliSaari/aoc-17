732761d1d4d2fe89da2dea0df71785619efe936c
EemeliSaari <saari.eemeli@gmail.com>
2017-12-12 17:11:07 +0200

    Skeletons for day 11 and 12

---
+++ day11/hex_ed.R
@@ -0,0 +1,14 @@
+# Advent of code 2017
+# Day 11
+
+
+main <- function(){
+  start = Sys.time()
+  
+  # Reads the commandline argument
+  argv <- commandArgs(TRUE)
+  
+  sprintf("Program took: %f", Sys.time()-start)
+}
+
+main()
