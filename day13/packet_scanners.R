d0dfa98e0112e154957fcd1899687a8f749588a2
EemeliSaari <saari.eemeli@gmail.com>
2017-12-14 17:53:52 +0200

    Skeleton for day 13-14
    Initial code for day13 and 14 to start working on.

---
+++ day13/packet_scanners.R
@@ -0,0 +1,14 @@
+# Advent of code 2017
+# Day 13
+
+
+main <- function(){
+  start = Sys.time()
+  
+  # Reads the commandline argument
+  argv <- commandArgs(TRUE)
+  
+  sprintf("Program took: %f", Sys.time()-start)
+}
+
+main()
