87be7a62a5376c5e260393ab6ebecc305662fc84
EemeliSaari <saari.eemeli@gmail.com>
2017-12-17 14:42:09 +0200

    Skeletons for day 16-17

---
+++ day16/permutation_promenade.R
@@ -0,0 +1,18 @@
+# Advent of code 2017
+# Day 16
+
+
+main <- function(){
+  start = Sys.time()
+  
+  # Reads the commandline argument
+  argv <- commandArgs(TRUE)
+  
+  input <- readLines(argv[1])
+  
+  print(input)
+  
+  sprintf("Program took: %f", Sys.time()-start)
+}
+
+main()
