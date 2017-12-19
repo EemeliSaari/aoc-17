de1a62c1582259a4b1e4522a4a6b8ac506eb77f6
EemeliSaari <saari.eemeli@gmail.com>
2017-12-19 17:43:08 +0200

    Day 19 skeleton
    Skeleton code for day 19

---
+++ day19/series_of_tubes.R
@@ -0,0 +1,15 @@
+# Advent of code 2017
+# Day 19
+
+
+main <- function(){
+  
+  start = Sys.time()
+  
+  # Reads the commandline argument
+  argv <- commandArgs(TRUE)
+  
+  sprintf("Program took: %f", Sys.time()-start)
+}
+
+main()
