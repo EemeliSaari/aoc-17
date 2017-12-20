2f8376585b7bfa932d2c507a451ffb1c99d7d1b0
EemeliSaari <saari.eemeli@gmail.com>
2017-12-20 11:00:52 +0200

    Finished day19
    Finished code for day 19

--- day19/series_of_tubes.R
+++ day19/series_of_tubes.R
@@ -1,5 +1,44 @@
 # Advent of code 2017
 # Day 19
+# Input was a maze
+
+
+navigate <- function(data){
+  'Navigate through the maze'
+  
+  ind <- c(1, match("|", data[1,]))
+  dir <- c(1,0)
+  
+  collection <- c()
+
+  current <- "|"
+  steps <- 0
+  repeat{
+    if(data[ind[1],ind[2]] %in% LETTERS)
+      collection = c(collection, data[ind[1],ind[2]])
+    else if(data[ind[1],ind[2]] == "+"){
+      # determine if travelling vertically or horizontally
+      if(dir[1] != 0){
+        slice <- data[ind[1], ((ind[2]-1):(ind[2]+1))]
+        dir <- c(0, (-2 + match("-", slice)))
+      }
+      else{
+        slice <- data[((ind[1]-1):(ind[1]+1)), ind[2]]
+        dir <- c((-2 + match("|", slice)), 0)
+      }
+    }
+    steps = steps + 1
+    # Check if the next is a dead end
+    if(data[(ind[1]+dir[1]),(ind[2]+dir[2])] == " ")
+      break
+    # Move otherwise
+    ind <- ind + dir
+  }
+  # Phase 1
+  print(paste(collection, collapse = ""))
+  # Phase 2
+  print(steps)
+}
 
 
 main <- function(){
@@ -8,6 +47,15 @@ main <- function(){
   
   # Reads the commandline argument
   argv <- commandArgs(TRUE)
+  
+  rawData <- readLines(argv[1])
+  
+  # Convert the txt file into a string matrix
+  data <- matrix(data=unlist(strsplit(rawData,"")), 
+                 nrow = length(unlist(strsplit(rawData[1],""))), 
+                 ncol = length(rawData), byrow = TRUE)
+
+  navigate(data)
   
   sprintf("Program took: %f", Sys.time()-start)
 }
